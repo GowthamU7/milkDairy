375a234bcb7630f8d3e3e26d0446b82ec27343a0
gowtham123-pro <gowthammdb7@gmail.com>
2022-08-05 23:19:32 +0530

    first milkDairy v1

--- dbConnection/dbConnection.js
+++ dbConnection/dbConnection.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/milkOrderDb").
+mongoose.connect("mongodb://sai:saisai99122@@cluster0-shard-00-00.0fn1n.mongodb.net:27017,cluster0-shard-00-01.0fn1n.mongodb.net:27017,cluster0-shard-00-02.0fn1n.mongodb.net:27017/milkDairy?ssl=true&replicaSet=atlas-12uddp-shard-0&authSource=admin&retryWrites=true&w=majority").
 then((vl)=>console.log("Connected to Database")).
 catch((reason)=>{console.log(reason)})
